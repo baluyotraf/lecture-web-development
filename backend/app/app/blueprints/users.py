8b5227225440df757f1d2f80d9010a8bb64464ca
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-15 18:20:27 +0800

    Added endpoints for user ops

---
+++ backend/app/app/blueprints/users.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, jsonify, request
+from ..models import db, User, UserSchema
+from ..auth import requires_user, failed_auth_response
+
+
+blueprint = Blueprint('users', __name__, url_prefix='/users')
+schema = UserSchema()
+_create_user_fields = ['username', 'password']
+
+
+@blueprint.route('/', methods=['POST'])
+def create_users():
+    user_details = request.get_json()
+    user = User(**{field: user_details[field]
+                   for field in _create_user_fields})
+    db.session.add(user)
+    db.session.commit()
+    user_json = schema.dump(user).data
+    return jsonify(user_json), 201, {'Location': 'wew'}
+
+
+@blueprint.route('/<int:id_>', methods=['GET'])
+@requires_user
+def view_user(user, id_):
+    print('id:', id_)
+    print('user:', user)
+    if user.id == id_:
+        user_json = schema.dump(user).data
+        return jsonify(user_json), 200
+    else:
+        return failed_auth_response()
+
