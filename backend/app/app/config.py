af645333b7b4347d6a3096e2dc9a7f655f6a8491
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-15 17:35:47 +0800

    Added functions for authentication

--- backend/app/app/config.py
+++ backend/app/app/config.py
@@ -4,3 +4,4 @@ from flask_env import MetaFlaskEnv
 class DefaultConfig(metaclass=MetaFlaskEnv):
     SQLALCHEMY_DATABASE_URI = 'mysql://user:user@database/db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ITSDANGEROUS_SECRET_KEY = 'hello'
